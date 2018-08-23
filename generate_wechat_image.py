6d8905038262c57b97b8b54eaea51a8da3042777
赵珂飞 <546109656@qq.com>
2018-08-23 13:08:13 +0800

    update

--- generate_wechat_image.py
+++ generate_wechat_image.py
@@ -7,20 +7,16 @@ import itchat
 from PIL import Image
 
 
-# 获取好友信息并取出图片
 def get_friend_imgs(save_path, get_img_nums=100):
-    # 创建存储图片路径
     if os.path.exists(save_path):
         save_path = input(u'该路径已存在，请输入其他目录路径: ')
     os.mkdir(save_path)
-    # 获取好友列表
     friends = itchat.get_friends()
     if get_img_nums > len(friends):
         get_img_nums = len(friends)
         print(u'需要获取的图片数量大于好友数量，取好友数量： %s' % len(friends))
     for num, friend in enumerate(friends):
         friend_name = friend['NickName'] or friend['UserName']
-        # 好友名字中带空格的用下划线替代
         friend_name = re.sub(r'[\s+]', '_', friend_name)
         friend_img = itchat.get_head_img(userName=friend['UserName'])
         with open(save_path + '/' + friend_name + str(num+1).zfill(3) + '.jpg', 'wb') as f:
@@ -32,16 +28,11 @@ def get_friend_imgs(save_path, get_img_nums=100):
 
 
 def generate_image(path, gen_filename='multi_img', row_num=10):
-    # 确定文件中可用的拼接图片数量
     images = os.listdir(path)
-    # 图片缩略尺寸
     slide_size = int(640/row_num)
     thum_size = (slide_size, slide_size)
-    # 创建画布
     toImage = Image.new('RGBA', (640, 640))
-    # 创建画布游标
     x = 0; y = 0
-    # 未写入的图片
     invilid_imgs = []
     for num, img in enumerate(images):
         if img.endswith('.jpg'):
@@ -53,7 +44,6 @@ def generate_image(path, gen_filename='multi_img', row_num=10):
                 print(u'%s 未写入' % img)
                 invilid_imgs.append(img)
                 continue
-            # 每行放`row_num`数量图片后，每个图片的尺寸
             if im.size != thum_size:
                 thum_im = im.resize(thum_size, Image.ANTIALIAS)
             else:
@@ -64,7 +54,6 @@ def generate_image(path, gen_filename='multi_img', row_num=10):
                 x = 0
                 y += 1
     print(u'未写入的图片有: {}'.format(' @|@ '.join(invilid_imgs)))
-    # 保存图片
     toImage.save(path + '/' + gen_filename + '.jpg')
     print(u'生成的文件位于: {}; 名为: {}'.format(path, gen_filename + '.jpg'))
 
